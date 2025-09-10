a80cdfbe59d6d6e51c7241aa968fc01403812188
IngrainedFire <seankeaganc@gmail.com>
2025-09-10 13:39:00 +0200

    Fixed Sign In Responsiveness Bug

--- components/auth/SignInForm.vue
+++ components/auth/SignInForm.vue
@@ -19,19 +19,22 @@ const handleSignIn = async (event: Event) => {
 
 <template>
   
-  <form @submit="handleSignIn" class=" flex flex-col flex-1 w-full max-w-[500px] p-4 mb-10 mx-auto">
-    <h1 class="text-center text-[24px] mb-4">Sign In</h1>
-    <div class="mb-4">
-      <label for="email" class="block mb-1">Email</label>
-      <input v-model="email" type="email" id="email" required class="w-full border-2 px-2 py-1 rounded-[10px]" />
+  <form @submit="handleSignIn" class="flex flex-col flex-1 w-full max-w-[500px] p-4 mb-10 mx-auto">
+    <div class="flex flex-col flex-grow mt-[20%] lg:mt-[15%] md:mt-[10%]">
+      <h1 class="text-center text-[24px] mb-4">Sign In</h1>
+      <div class="mb-4">
+        <label for="email" class="block mb-1">Email</label>
+        <input v-model="email" type="email" id="email" required class="w-full border-2 px-2 py-1 rounded-[10px]" />
+      </div>
+
+      <div class="mb-4">
+        <label for="password" class="block mb-1">Password</label>
+        <input v-model="password" type="password" id="password" required class="w-full border-2 px-2 py-1 rounded-[10px]" />
+      </div>
     </div>
 
-    <div class="mb-4">
-      <label for="password" class="block mb-1">Password</label>
-      <input v-model="password" type="password" id="password" required class="w-full border-2 px-2 py-1 rounded-[10px]" />
-    </div>
 
-    <button type="submit" class="bg-[#FF5787] text-white px-4 py-2 rounded-[10px] w-full mt-auto">
+    <button type="submit" class="bg-[#FF5787] text-white px-4 py-2 rounded-[10px] w-full mt-auto mb-[20%]">
       Sign In
     </button>
   </form>
